c7980838f0638944bd4d927539e53a89c00abd50
mouna <23018@esp.mr>
2024-07-25 13:40:47 +0200

    folder for backend

---
+++ gestiontransactionbackend/src/main/java/com/example/gestiontransactionbackend/GestiontransactionbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestiontransactionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestiontransactionbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestiontransactionbackendApplication.class, args);
+	}
+
+}
---
+++ gestiontransactionbackend/src/test/java/com/example/gestiontransactionbackend/GestiontransactionbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestiontransactionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestiontransactionbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
